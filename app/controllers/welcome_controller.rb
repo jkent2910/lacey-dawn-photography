ac0d0da824c7a14dc98db2bd98d1d70353ff1e0f
Julie Kent <jkent2910@gmail.com>
2016-11-20 17:07:55 -0600

    static pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,6 +8,45 @@ class WelcomeController < ApplicationController
   def about
   end
 
+  def newborns
+  end
+
+  def toddlers
+  end
+
+  def maternity
+  end
+
+  def landscapes
+  end
+
+  def city
+  end
+
+  def engagements
+  end
+
+  def weddings
+  end
+
+  def seniors
+  end
+
+  def events
+  end
+
+  def minisessions
+  end
+
+  def testimonials
+  end
+
+  def pricing
+  end
+
+  def contact_me
+  end
+
   def contact
     ContactMailer.contact_form(params[:name], params[:email], params[:phone], params[:question]).deliver_now
 
